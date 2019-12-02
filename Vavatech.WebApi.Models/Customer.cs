1099b3d54d5396d0a5de4c802bb50f00236700d0
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2019-12-02 11:03:13 +0100

    Add project files.

---
+++ Vavatech.WebApi.Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Vavatech.WebApi.Models
+{
+
+
+    public class Customer : EntityBase
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public decimal Salary { get; set; }
+        public Gender Gender { get; set; }
+        public bool IsRemoved { get; set; }
+
+        public Customer()
+        {
+            Salary = 100;
+            Gender = Gender.Female;
+            IsRemoved = false;
+        }
+
+    }
+}
