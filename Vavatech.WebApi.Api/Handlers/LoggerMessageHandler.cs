b2c794bc731ef3b7633b3a941b9d91c8e0085693
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2019-12-04 09:09:34 +0100

    Refaktoring

--- Vavatech.WebApi.Api/Handlers/LoggerMessageHandler.cs
+++ Vavatech.WebApi.Api/Handlers/LoggerMessageHandler.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
@@ -9,26 +8,6 @@ using System.Web;
 
 namespace Vavatech.WebApi.Api.Handlers
 {
-    public class SecretKeyMessageHandler : DelegatingHandler
-    {
-        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-        {
-            if (request.Headers.TryGetValues("secret-key", out IEnumerable<string> values))
-            {
-                return base.SendAsync(request, cancellationToken);
-            }
-            else
-            {
-                HttpResponseMessage response = new HttpResponseMessage(System.Net.HttpStatusCode.Unauthorized);
-
-                return Task.FromResult(response);
-            }
-
-            
-
-           
-        }
-    }
 
     public class LoggerMessageHandler : DelegatingHandler 
     {
