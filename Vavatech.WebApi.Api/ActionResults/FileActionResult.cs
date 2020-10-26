350be93f77302d05e2dcdcb73506249a2cb407e3
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-10-26 13:59:58 +0100

    Przykład pobierania plików zip z usługi sieciowej

---
+++ Vavatech.WebApi.Api/ActionResults/FileActionResult.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace Vavatech.WebApi.Api.ActionResults
+{
+    public class FileActionResult : IHttpActionResult
+    {
+        private readonly Stream stream;
+        private readonly string filename;
+        private readonly HttpRequestMessage httpRequestMessage;
+
+        public FileActionResult(Stream stream, HttpRequestMessage request, string filename)
+        {
+            this.stream = stream;
+            httpRequestMessage = request;
+            this.filename = filename;
+        }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            HttpResponseMessage httpResponseMessage = httpRequestMessage.CreateResponse(HttpStatusCode.OK);
+            httpResponseMessage.Content = new StreamContent(stream);
+            httpResponseMessage.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
+            httpResponseMessage.Content.Headers.ContentDisposition.FileName = filename;
+            httpResponseMessage.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
+
+            return Task.FromResult(httpResponseMessage);
+        }
+
+     
+    }
+}
---
+++ Vavatech.WebApi.Api/Controllers/FilesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Vavatech.WebApi.Api.ActionResults;
+
+namespace Vavatech.WebApi.Api.Controllers
+{
+    [RoutePrefix("api/files")]
+    public class FilesController : ApiController
+    {
+        [HttpGet]
+        [Route("{filename}.{ext}")]
+        [Route("{filename}")]
+        public IHttpActionResult Get(string filename)
+        {
+            filename += ".zip";
+
+            string directory = HttpContext.Current.Server.MapPath("~/");
+
+            string path = Path.Combine(directory, "StaticFiles", filename);
+
+            var bytes = File.ReadAllBytes(path);
+
+            Stream stream = new MemoryStream(bytes);
+
+            return new FileActionResult(stream, this.Request, filename);
+        }
+    }
+}
